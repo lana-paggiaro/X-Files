21823bd9e02c75936ffc4684e602310d1c0ccdf0
Lua-da-tarde <luanna@paggiaro.com.br>
2022-11-18 08:15:14 -0300

    comentei os caminhos e ajeitei o atualizar

--- projetoX/src/main/java/com/example/projetoX/controller/AlienController.java
+++ projetoX/src/main/java/com/example/projetoX/controller/AlienController.java
@@ -15,70 +15,85 @@ public class AlienController {
     @Autowired
     AlienRepository alienR;
 
+    /*(localhost:8080/apiAlien/insertAlien)*/
     @PostMapping ("/insertAlien")
     public void insertAlien(@RequestBody Alien alien){
         alienR.save(alien);
     }
 
+    /*(localhost:8080/apiAlien/updateAlien)*/
+    @PutMapping("/updateAlien")
     public void updateAlien(@RequestBody Alien alien){
         alienR.save(alien);
     }
 
+    /*(localhost:8080/apiAlien/deleteAlien)*/
     @DeleteMapping("/deleteAlien")
     public void deleteAlien(@RequestBody Alien alien){
         alienR.delete(alien);
     }
 
+    /*(localhost:8080/apiAlien/allAliens)*/
     @GetMapping(value="/allAliens")
     public List<Alien> searchAll (){
         return alienR.findAll();
     }
 
+    /*(localhost:8080/apiAlien/searchCode/{code})*/
     @GetMapping ("/searchCode/{code}")
     public Optional<Alien> searchByCode(@PathVariable(value="code")int code) {
         return alienR.findById(code);
     }
 
+    /*(localhost:8080/apiAlien/searchType/{type})*/
     @GetMapping("/searchType/{type}")
     public List<Alien> listByType(@PathVariable(value="type") String type){
         return alienR.findByType(type);
     }
 
+    /*(localhost:8080/apiAlien/searchDescription/{description})*/
     @GetMapping("/searchDescription/{description}")
     public List<Alien> listByDescription (@PathVariable(value="description") String description){
         return alienR.findByDescription(description);
     }
 
+    /*(localhost:8080/apiAlien/searchPrice/{price})*/
     @GetMapping("/searchPrice/{price}")
     public List<Alien> listByPrice (@PathVariable(value="price") double price){
         return alienR.findByPrice(price);
     }
 
+    /*(localhost:8080/apiAlien/initialType/{type})*/
     @GetMapping("/initialType/{type}")
     public List<Alien> listByTypeInitial (@PathVariable(value="type") String type){
         return alienR.findByTypeInitial(type);
     }
 
+    /*(localhost:8080/apiAlien/initialDescription/{description})*/
     @GetMapping("/initialDescription/{description}")
     public List<Alien> listByDescriptionInitial(@PathVariable(value="description")String description){
         return alienR.findByDescriptionInitial(description);
     }
 
+    /*(localhost:8080/apiAlien/majorPrice/{price})*/
     @GetMapping("/majorPrice/{price}")
     public List<Alien> listByMajorPrice(@PathVariable(value="price")double price){
         return alienR.findByMajorPrice(price);
     }
 
+    /*(localhost:8080/apiAlien/minorPrice/{price})*/
     @GetMapping("/minorPrice/{price}")
     public List<Alien> listByMinorPrice(@PathVariable(value="price")double price){
         return alienR.findByMinorPrice(price);
     }
 
+    /*(localhost:8080/apiAlien/description/{description}/type/{type})*/
     @GetMapping("/description/{description}/type/{type}")
     public List<Alien> listByDescriptionAndType(@PathVariable(value="description")String description,@PathVariable(value="type")String type){
         return alienR.findByDescriptionAndType(description, type);
     }
 
+    /*(localhost:8080/apiAlien/description/{description}/price/{price})*/
     @GetMapping("/description/{description}/price/{price}")
     public List<Alien> listByDescriptionAndPrice(@PathVariable(value="description")String description,@PathVariable(value="price")double price){
         return alienR.findByDescriptionAndPrice(description, price);
--- projetoX/src/main/java/com/example/projetoX/controller/CustomerController.java
+++ projetoX/src/main/java/com/example/projetoX/controller/CustomerController.java
@@ -16,55 +16,67 @@ public class CustomerController {
     @Autowired
     CustomerRepository customerRepository;
 
+    /*(localhost:8080/apiCustomer/insertCustomer)*/
     @PostMapping("/insertCustomer")
     public void insertCustomer(@RequestBody Customer customer){
         customerRepository.save(customer);
     }
 
+    /*(localhost:8080/apiCustomer/updateCustomer)*/
+    @PutMapping("/updateCustomer")
     public void updateCustomer(@RequestBody Customer cust){
         customerRepository.save(cust);
     }
 
+    /*(localhost:8080/apiCustomer/allCustomers)*/
     @GetMapping(value="/allCustomers")
     public List<Customer> searchAll (){
         return customerRepository.findAll();
     }
 
-    @DeleteMapping
+    /*(localhost:8080/apiCustomer/deleteCustomer)*/
+    @DeleteMapping("/deleteCustomer")
     public void deleteCustomer(@RequestBody Customer cust){
         customerRepository.delete(cust);
     }
 
+    /*(localhost:8080/apiCustomer/searchName/{name})*/
     @GetMapping("/searchName/{name}")
     public List<Customer> listByName(@PathVariable(value="name") String name){
         return customerRepository.findByName(name);
     }
 
+    /*(localhost:8080/apiCustomer/searchEmail/)*/
     @GetMapping("/searchEmail/{email}")
     public List<Customer> listByEmail(@PathVariable(value="email") String email){
         return customerRepository.findByEmail(email);
     }
 
+    /*(localhost:8080/apiCustomer/searchCustomer/{codec})*/
     @GetMapping ("/searchCode/{codec}")
     public Optional<Customer> searchByCode(@PathVariable(value="codec")int codec) {
         return customerRepository.findById(codec);
     }
 
+    /*(localhost:8080/apiCustomer/initialName/{name})*/
     @GetMapping("/initialName/{name}")
     public List<Customer> listByNameInitial(@PathVariable(value="name") String name){
         return customerRepository.findByNameInitial(name); /*fé*/
     }
 
+    /*(localhost:8080/apiCustomer/initialEmail/{email})*/
     @GetMapping("/initialEmail/{email}")
     public List<Customer> listByEmailInitial (@PathVariable(value="email") String email){
         return customerRepository.findByEmailInitial(email);
     }
 
+    /*(localhost:8080/apiCustomer/majorCode/{codec})*/
     @GetMapping("/majorCode/{codec}")
     public List<Customer> listByMajorCode(@PathVariable(value="codec") int codec){
         return customerRepository.findByMajorCode(codec);
     }
 
+    /*(localhost:8080/apiCustomer/name/{name}/email/{email})*/
     @GetMapping("/name/{name}/email/{email}")
     public List<Customer> listByNameAndEmail(@PathVariable(value="name")String name,@PathVariable(value="email")String email){
         return customerRepository.findByNameAndEmail(name, email);
